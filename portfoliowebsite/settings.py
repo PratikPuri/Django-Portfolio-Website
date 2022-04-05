69526ce5f56d997729efa1bda5bf42c660a2277e
pat_nil <52376526+PratikPuri@users.noreply.github.com>
2022-04-06 00:22:12 +0530

    commit

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -120,17 +120,30 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join (BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+
 # STATIC_ROOT = 'staticfiles'
 
+#---------
+# STATIC_ROOT = os.path.join (BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static'),
+# ]
+
+# MEDIA_ROOT = os.path.join (BASE_DIR, 'static/images')
+# MEDIA_URL = '/static/images/'
+#---------
+
+# MEDIA_ROOT = os.path.join (BASE_DIR, 'staticfiles/images') #Used to specify where the user uploaded stuff goes
+
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
-
+STATIC_ROOT = os.path.join (BASE_DIR, 'staticfiles')
+MEDIA_URL = 'static/images/'
 MEDIA_ROOT = os.path.join (BASE_DIR, 'static/images')
-# MEDIA_ROOT = os.path.join (BASE_DIR, 'staticfiles/images') #Used to specify where the user uploaded stuff goes
-MEDIA_URL = '/static/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
